8b74064ed00d7148fd6a0c464dd937cf0a7c40fe
yexianghu <yexianghu@gmail.com>
2018-08-23 22:56:55 +0800

    init commit

---
+++ aac/src/main/java/com/yezi/aac/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yezi.aac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
